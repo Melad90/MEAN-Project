29f7a53790b600e09a4ba8e9bac8030610d1742a
melad <meladfrancis@hotmail.com>
2018-07-24 22:33:36 +0200

    ny version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'marnarsay';
+  storedPost =  [];
+  onPostAdded(post) {
+    this.storedPost.push(post);
+  }
 }
