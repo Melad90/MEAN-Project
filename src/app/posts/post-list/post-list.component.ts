da353174d9d02995077869b23d6d40650c7c90c6
Batta <melad-francis.batta@sogeti.com>
2018-08-14 22:11:10 +0200

    update

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, ViewChildren, QueryList, ElementRef } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {Post} from '../post.model';
 import { PostsService } from '../posts.service';
 import { Subscription } from 'rxjs';
@@ -16,20 +16,10 @@ export class PostListComponent implements OnInit, OnDestroy {
   totalPosts = 3;
   postPerPage = 3;
   pageSize = [24, 30, 60, 90];
-
-  @ViewChildren('pages') pages: QueryList<any>;
-  itemsPerPage = 3;
-  numberOfVisiblePaginators = 10;
-  numberOfPaginators: number;
-  paginators: Array<any> = [];
-  activePage = 1;
-  firstVisibleIndex = 1;
-  lastVisibleIndex: number = this.itemsPerPage;
-  firstVisiblePaginator = 0;
-  lastVisiblePaginator = this.numberOfVisiblePaginators;
+  currentPage = 0;
 
   private postsSub: Subscription;
-  constructor(public postsService: PostsService, private el: ElementRef) {}
+  constructor(public postsService: PostsService) {}
 
   ngOnInit() {
     this.postsService.getPosts();
@@ -40,68 +30,12 @@ export class PostListComponent implements OnInit, OnDestroy {
     });
   }
 
-  changePage(pageData: any) {
-    if (event.target.text >= 1 && event.target.text <= this.numberOfPaginators) {
-      this.activePage = +event.target.text;
-      this.firstVisibleIndex = this.activePage * this.itemsPerPage - this.itemsPerPage + 1;
-      this.lastVisibleIndex = this.activePage * this.itemsPerPage;
-    }
+  pageEvent: PageEvent;
+
+  setPageSizeOptions(setPageSizeOptionsInput: string) {
+    this.pageSize = setPageSizeOptionsInput.split(',').map(str => +str);
   }
-
-  nextPage(event: any) {
-    if (this.pages.last.nativeElement.classList.contains('active')) {
-      if ((this.numberOfPaginators - this.numberOfVisiblePaginators) >= this.lastVisiblePaginator) {
-        this.firstVisiblePaginator += this.numberOfVisiblePaginators;
-      this.lastVisiblePaginator += this.numberOfVisiblePaginators;
-      } else {
-        this.firstVisiblePaginator += this.numberOfVisiblePaginators;
-      this.lastVisiblePaginator = this.numberOfPaginators;
-      }
-    }
-
-    this.activePage += 1;
-    this.firstVisibleIndex = this.activePage * this.itemsPerPage - this.itemsPerPage + 1;
-    this.lastVisibleIndex = this.activePage * this.itemsPerPage;
-  }
-
-  previousPage(event: any) {
-    if (this.pages.first.nativeElement.classList.contains('active')) {
-      if ((this.lastVisiblePaginator - this.firstVisiblePaginator) === this.numberOfVisiblePaginators)  {
-        this.firstVisiblePaginator -= this.numberOfVisiblePaginators;
-        this.lastVisiblePaginator -= this.numberOfVisiblePaginators;
-      } else {
-        this.firstVisiblePaginator -= this.numberOfVisiblePaginators;
-        this.lastVisiblePaginator -= (this.numberOfPaginators % this.numberOfVisiblePaginators);
-      }
-    }
-
-    this.activePage -= 1;
-    this.firstVisibleIndex = this.activePage * this.itemsPerPage - this.itemsPerPage + 1;
-    this.lastVisibleIndex = this.activePage * this.itemsPerPage;
-  }
-
-  firstPage() {
-    this.activePage = 1;
-    this.firstVisibleIndex = this.activePage * this.itemsPerPage - this.itemsPerPage + 1;
-    this.lastVisibleIndex = this.activePage * this.itemsPerPage;
-    this.firstVisiblePaginator = 0;
-    this.lastVisiblePaginator = this.numberOfVisiblePaginators;
-  }
-
-  lastPage() {
-    this.activePage = this.numberOfPaginators;
-    this.firstVisibleIndex = this.activePage * this.itemsPerPage - this.itemsPerPage + 1;
-    this.lastVisibleIndex = this.activePage * this.itemsPerPage;
-
-    if (this.numberOfPaginators % this.numberOfVisiblePaginators === 0) {
-      this.firstVisiblePaginator = this.numberOfPaginators - this.numberOfVisiblePaginators;
-      this.lastVisiblePaginator = this.numberOfPaginators;
-    } else {
-      this.lastVisiblePaginator = this.numberOfPaginators;
-      this.firstVisiblePaginator = this.lastVisiblePaginator - (this.numberOfPaginators % this.numberOfVisiblePaginators);
-    }
-  }
-
+  
   onDelete(postId: string) {
     this.postsService.deletePost(postId);
   }
