c412f4a8810ab50624547918c1f8fd28c7d8e722
Batta <melad-francis.batta@sogeti.com>
2018-09-23 15:19:18 +0200

    new

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,15 +2,21 @@ import {Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthData } from './auth-data.model';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
     private token: string;
+    private authStatusListener = new Subject<boolean>();
 
     constructor(private http: HttpClient, private router: Router) {}
 
     getToken(){
         return this.token;
+    }
+
+    getAuthStatusListener(){
+        return this.authStatusListener.asObservable();
     }
 
     createUser(email: string, password: string){
@@ -27,7 +33,7 @@ export class AuthService {
         .subscribe(response => {
             const token = response.token;
             this.token = token;
-            //this.router.navigateByUrl('/');
+            this.authStatusListener.next(true);
             console.log(response);
         });
     }
